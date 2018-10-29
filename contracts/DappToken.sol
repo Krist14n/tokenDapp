55076e2008346b9121c9c3709c60de06cb69426b
kristiantapia <kristian.tapia@blockchaingroup.io>
2018-10-29 15:01:19 -0700

    transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,30 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
-    // Constructor
     uint256 public totalSupply;
+    string public name = "Dapp Token";
+    string public symbol = "XAAP";
+    string public standard = "Dapp token v1.0";
 
-    constructor () public {
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
+    mapping(address => uint256) public balanceOf;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        
     }
 
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
